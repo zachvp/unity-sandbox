8c14836f557b2abccf9670f43fefa7c7e3eefe99
zachvp <vegaperk@gmail.com>
2022-09-22 14:59:19 -0400

    poc for generic grounded trigger

---
+++ Assets/Scripts/VolumeTrigger.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class VolumeTrigger : MonoBehaviour
+{
+    public bool isActive;
+    public LayerMask mask;
+    new public Collider2D collider;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        UpdateState();
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        UpdateState();
+    }
+
+    private void UpdateState()
+    {
+        var filter = new ContactFilter2D();
+        var colliders = new Collider2D[1];
+
+        filter.useLayerMask = true;
+        filter.layerMask = mask;
+
+        var result = collider.OverlapCollider(filter, colliders);
+
+        isActive = result > 0;
+    }
+}
