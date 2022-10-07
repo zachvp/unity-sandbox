612850171af9129251269a6102a4d7bf878bd5a4
zachvp <vegaperk@gmail.com>
2022-10-07 12:16:07 -0600

    commit experiment with visual scripting

--- Assets/Scripts/Archive/Emitter.cs
+++ Assets/Scripts/Archive/Emitter.cs
@@ -5,13 +5,17 @@ using UnityEngine.InputSystem;
 public class Emitter : MonoBehaviour
 {
     public UnityEvent testEvent;
+    //public 
 
     public void Update()
     {
         if (Keyboard.current.enterKey.wasPressedThisFrame)
         {
             testEvent.Invoke();
+
+            Animator blah;
             
+            //blah.sta
         }
     }
 }
