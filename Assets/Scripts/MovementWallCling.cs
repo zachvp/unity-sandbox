6d845d4ad485d3c9e78c77aeae82e342317d1ef8
zachvp <vegaperk@gmail.com>
2022-10-12 14:36:54 -0600

    wall cling bug, commit prior to hierarchy changes

--- Assets/Scripts/MovementWallCling.cs
+++ Assets/Scripts/MovementWallCling.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using System.Collections;
 
 public class MovementWallCling : MonoBehaviour
 {
+    public bool stateCling;
+
     public bool Trigger(bool lhsBlocked, bool rhsBlocked, bool grounded, short inputAxis, Rigidbody2D rigidbody)
     {
-        var leftCondition = inputAxis > 0 && rhsBlocked;
-        var rightCondition = inputAxis < 0 && lhsBlocked;
-        var result = Mathf.Abs(inputAxis) > 0 && !grounded && rigidbody.velocity.y < 0;
+        var rightCondition = inputAxis > 0 && rhsBlocked;
+        var leftCondition = inputAxis < 0 && lhsBlocked;
+        var result = Mathf.Abs(inputAxis) > 0 && !grounded;// && rigidbody.velocity.y < 0.1f;
 
-        result &= leftCondition || rightCondition;
+        //if (leftCondition)
+        //{
+        //    Debug.LogFormat("zvp: result: {0}", result);
+        //}
+
+        //Debug.LogFormat("zvp: left condition: {0}", leftCondition);
+        result &= leftCondition;// || rightCondition;
 
         Debug.LogFormat("zvp: result: {0}", result);
 
@@ -22,6 +31,10 @@ public class MovementWallCling : MonoBehaviour
         newVelocity.y = 0;
 
         rigidbody.velocity = newVelocity;
-        rigidbody.Sleep();
+    }
+
+    public void Reset(Rigidbody2D rigidbody)
+    {
+        rigidbody.WakeUp();
     }
 }
