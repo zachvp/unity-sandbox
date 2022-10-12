10a095fea927cc48cf9c57184798e0db1c6b775e
zachvp <vegaperk@gmail.com>
2022-10-12 12:37:08 -0600

    poc for wall cling

--- Assets/Scripts/MovementWallCling.cs
+++ Assets/Scripts/MovementWallCling.cs
@@ -2,12 +2,26 @@ using UnityEngine;
 
 public class MovementWallCling : MonoBehaviour
 {
-    public bool Trigger(bool lhsBlocked, bool rhsBlocked, bool grounded, short inputAxis)
+    public bool Trigger(bool lhsBlocked, bool rhsBlocked, bool grounded, short inputAxis, Rigidbody2D rigidbody)
     {
-        var result = Mathf.Abs(inputAxis) > 0 && !grounded && (lhsBlocked || rhsBlocked);
+        var leftCondition = inputAxis > 0 && rhsBlocked;
+        var rightCondition = inputAxis < 0 && lhsBlocked;
+        var result = Mathf.Abs(inputAxis) > 0 && !grounded && rigidbody.velocity.y < 0;
+
+        result &= leftCondition || rightCondition;
 
         Debug.LogFormat("zvp: result: {0}", result);
 
         return result;
+    }
+
+    public void Cling(Rigidbody2D rigidbody)
+    {
+        var newVelocity = rigidbody.velocity;
+
+        newVelocity.y = 0;
+
+        rigidbody.velocity = newVelocity;
+        rigidbody.Sleep();
     }
 }
