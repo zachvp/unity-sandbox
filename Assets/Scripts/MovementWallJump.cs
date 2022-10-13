08f934ada8f90f7ffb5ec34f6edeeb72f3123340
zachvp <vegaperk@gmail.com>
2022-10-13 15:41:59 -0600

    base commit before attempting wall jump

---
+++ Assets/Scripts/MovementWallJump.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+/*
+    onJumpInput
+        if movementwallcling.trigger()
+            modVelocity.TriggerY(wallJumpValue)
+*/
+
+public class MovementWallJump : MonoBehaviour
+{
+    public bool Trigger(MovementWallCling wallCling)
+    {
+        var result = false;
+
+        //if (wallCling.Trigger())
+        // todo: u know
+
+        return result;
+    }
+}
