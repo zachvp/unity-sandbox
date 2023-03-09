c238d103fb3de63094d658d36b3bfd7fb95f0354
zachvp <vegaperk@gmail.com>
2023-03-09 02:09:33 -0700

    sync ball rotation across game objects

--- Assets/Scripts/PCHandMotor.cs
+++ Assets/Scripts/PCHandMotor.cs
@@ -3,13 +3,14 @@ using UnityEngine.InputSystem;
 
 public class PCHandMotor : MonoBehaviour
 {
+    // TODO: separate pickup stuff into separate class
     public InputHandlerButton inputGrip;
     public InputHandlerButton inputThrow;
     public InputHandlerAnalogStick inputGesture;
     public TriggerVolume grabTrigger;
-    public GameObject pickupObject;
-    public GameObject heldObject;
-    public GameObject releasedObject;
+    public Rigidbody2D pickupObject;
+    public Rigidbody2D heldObject;
+    public Rigidbody2D releasedObject;
     public CoreBody hand;
     public MovementRadial movementHeldPickup;
     public float interactionBlockDelay;
@@ -35,19 +36,21 @@ public class PCHandMotor : MonoBehaviour
             {
                 var releasedBody = releasedObject.GetComponent<CoreBody>();
 
-                heldObject.SetActive(false);
+                heldObject.gameObject.SetActive(false);
                 releasedObject.transform.position = heldObject.transform.position;
-                releasedObject.SetActive(true);
+                releasedObject.gameObject.SetActive(true);
                 releasedBody.Trigger(hand.velocity);
+
                 state &= ~HandState.GRIP;
                 state |= HandState.BLOCKED;
 
                 StartCoroutine(CoreUtilities.DelayedTask(interactionBlockDelay, () =>
                 {
                     pickupObject.transform.position = releasedObject.transform.position;
-                    pickupObject.SetActive(true);
+                    pickupObject.gameObject.SetActive(true);
                     pickupObject.GetComponent<CoreBody>().Trigger(releasedBody.velocity);
-                    releasedObject.SetActive(false);
+                    releasedObject.gameObject.SetActive(false);
+
                     state &= ~HandState.BLOCKED;
                 }));
             }
@@ -62,8 +65,10 @@ public class PCHandMotor : MonoBehaviour
             {
                 if (state == HandState.NONE)
                 {
-                    heldObject.SetActive(true);
-                    pickupObject.SetActive(false);
+                    heldObject.gameObject.SetActive(true);
+                    heldObject.rotation = pickupObject.rotation;
+                    pickupObject.gameObject.SetActive(false);
+
                     state |= HandState.BLOCKED;
                     state |= HandState.GRIP;
 
@@ -76,17 +81,21 @@ public class PCHandMotor : MonoBehaviour
 
             if (state == HandState.GRIP)
             {
+                releasedObject.gameObject.SetActive(true);
+                heldObject.gameObject.SetActive(false);
                 releasedObject.transform.position = heldObject.transform.position;
-                releasedObject.SetActive(true);
-                heldObject.SetActive(false);
+                releasedObject.rotation = heldObject.rotation;
+
                 state |= HandState.BLOCKED;
                 state &= ~HandState.GRIP;
 
                 StartCoroutine(CoreUtilities.DelayedTask(interactionBlockDelay, () =>
                 {
+                    releasedObject.gameObject.SetActive(false);
+                    pickupObject.gameObject.SetActive(true);
                     pickupObject.transform.position = releasedObject.transform.position;
-                    releasedObject.SetActive(false);
-                    pickupObject.SetActive(true);
+                    pickupObject.rotation = releasedObject.rotation;
+
                     state &= ~HandState.BLOCKED;
                 }));
             }
