2fd14bafb5e0c03e232a040b62151b5559e53b66
zachvp <vegaperk@gmail.com>
2023-04-28 09:12:48 -0600

    add MyBox from github tagged zip
    use the 'conditional field' attribute from this lib =)

--- Assets/Scripts/MovementFollowTransform.cs
+++ Assets/Scripts/MovementFollowTransform.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using MyBox;
 
 public class MovementFollowTransform : MonoBehaviour
 {
     public Transform root;
     public bool usePhysics;
-    
+
+    [ConditionalField("usePhysics")]
     public Rigidbody2D body;
 
     public void Update()
