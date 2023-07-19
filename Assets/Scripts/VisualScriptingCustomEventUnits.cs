981308eee312ea2222bdf4b206862b4cde50a497
zachvp <vegaperk@gmail.com>
2023-07-19 13:33:53 -0600

    remove unused event units; regen nodes

--- Assets/Scripts/VisualScriptingCustomEventUnits.cs
+++ Assets/Scripts/VisualScriptingCustomEventUnits.cs
@@ -6,7 +6,8 @@ using UnityEngine.InputSystem;
 // This file declare custom events for the game. When you write your own event, don't forget to Regenerate Units through
 // the "Regenerate Unit" button in the Visual Script section of the Project Settings
 
-public sealed class InputButtonEvent : EventUnit<InputButtonArgs>
+// This class is unused and serves as a reference only.
+public sealed class REFInputButtonEvent : EventUnit<InputButtonArgs>
 {
     public static string Hook = EnumHelper.GetStringID(CustomHook.INPUT_BUTTON);
 
@@ -29,49 +30,7 @@ public sealed class InputButtonEvent : EventUnit<InputButtonArgs>
         flow.SetValue(action, args.action);
     }
 }
-
-public sealed class InputAxis1DEvent : EventUnit<InputAxis1DArgs>
-{
-    public static string Hook = EnumHelper.GetStringID(CustomHook.INPUT_BUTTON);
-
-    protected override bool register => true;
-    public override EventHook GetHook(GraphReference r) => Hook;
-
-    // todo: expose arg properties
-}
-
-public sealed class InputAxis2DEvent : EventUnit<InputAxis2DArgs>
-{
-    public static string Hook = EnumHelper.GetStringID(CustomHook.INPUT_AXIS2D);
-
-    protected override bool register => true;
-    public override EventHook GetHook(GraphReference r) => Hook;
-
-    // todo: expose arg properties
-}
 /* End input event units */
-
-/** COMMAND EVENTS */
-public sealed class CommandEvent : EventUnit<PCInputArgs>
-{
-    public static string Hook = EnumHelper.GetStringID(CustomHook.COMMAND);
-
-    protected override bool register => true;
-    public override EventHook GetHook(GraphReference r) => Hook;
-
-    public ValueOutput command { get; private set; }
-
-    protected override void Definition()
-    {
-        base.Definition();
-        command = ValueOutput<InputActionPhase>(nameof(command));
-    }
-
-    protected override void AssignArguments(Flow flow, PCInputArgs args)
-    {
-        flow.SetValue(command, args);
-    }
-}
 
 /**  REFERENCE: Machine events, independent of gameobject */
 [UnitCategory("Events/Core")]
