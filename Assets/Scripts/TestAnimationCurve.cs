65ab25421c9144eed78ba5c51ac600e7ab1ab43b
zachvp <vegaperk@gmail.com>
2023-07-19 19:09:34 -0600

    poc for animation curve driven ping pong movement

--- Assets/Scripts/TestAnimationCurve.cs
+++ Assets/Scripts/TestAnimationCurve.cs
@@ -7,10 +7,12 @@ using UnityEngine.SceneManagement;
 public class TestAnimationCurve : MonoBehaviour
 {
     public AnimationCurve curve;
+    public AnimationCurve yCurve;
     public float t;
+    public int multiplier = 1;
     public float totalTime;
     public float totalDistance;
-    public bool isLeft;
+    public bool inbound;
 
     public void Update()
     {
@@ -19,19 +21,38 @@ public class TestAnimationCurve : MonoBehaviour
             Debug.Log("move right");
             var newPos = transform.position;
             newPos.x = curve.Evaluate(t / totalTime) * totalDistance;
+            newPos.y = yCurve.Evaluate(t / totalTime) * totalDistance;
 
+            //distanceTraveled = Mathf.Abs((newPos - transform.position).magnitude);
             transform.position = newPos;
 
-            if (t < totalTime)
+            if (inbound)
+            {
+                t -= Time.deltaTime;
+
+                if (t < 0)
+                {
+                    inbound = false;
+                }
+            }
+            else
             {
                 t += Time.deltaTime;
+
+                if (t > totalTime)
+                {
+                    inbound = true;
+                }
             }
+
         }
+        // todo: movement goes back and forth with single key held
         if (Keyboard.current.leftArrowKey.isPressed)
         {
             Debug.Log("move left");
             var newPos = transform.position;
             newPos.x = curve.Evaluate(t / totalTime) * totalDistance;
+            newPos.y = yCurve.Evaluate(t / totalTime) * totalDistance;
 
             transform.position = newPos;
 
