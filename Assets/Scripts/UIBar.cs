3f453774582981538484454c4f2a6b7b1dbc8e3b
zachvp <vegaperk@gmail.com>
2023-08-09 18:49:41 +0800

    clamp UI t value

--- Assets/Scripts/UIBar.cs
+++ Assets/Scripts/UIBar.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class UIBar : MonoBehaviour
 {
     public RectTransform rect;
-    public float speed = 256;
     public bool shrink;
     public Vector2 sizeStart;
-    public Vector2 worldPosition;
 
     public float t;
     public float totalTime;
@@ -20,54 +18,21 @@ public class UIBar : MonoBehaviour
 
     public void Update()
     {
-        worldPosition = rect.position;
+        var newSize = rect.localScale;
 
         if ((t < totalTime && !shrink) || (t > 0 && shrink))
         {
-            var newSize = rect.localScale;
+            if (shrink)
+            {
+                t = Mathf.Clamp(t - Time.deltaTime, 0, totalTime);
+            }
+            else
+            {
+                t = Mathf.Clamp(t + Time.deltaTime, 0, totalTime);
+            }
 
             newSize.x = Mathf.Lerp(0, sizeStart.x, t / totalTime);
             rect.localScale = newSize;
-
-            if (shrink)
-            {
-                t -= Time.deltaTime;
-            }
-            else
-            {
-                t += Time.deltaTime;
-            }
         }
-
-    }
-
-    public void SimpleScale()
-    {
-        var newSize = rect.sizeDelta;
-
-        if (shrink)
-        {
-            if (rect.sizeDelta.x < 0)
-            {
-                newSize.x = 0;
-            }
-            else
-            {
-                newSize.x -= speed * Time.deltaTime;
-            }
-        }
-        else
-        {
-            if (rect.sizeDelta.x > sizeStart.x)
-            {
-                newSize.x = sizeStart.x;
-            }
-            else
-            {
-                newSize.x += speed * Time.deltaTime;
-            }
-        }
-
-        rect.sizeDelta = newSize;
     }
 }
